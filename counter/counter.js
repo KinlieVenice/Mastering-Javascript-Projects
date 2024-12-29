de514b5c3994f6079a9a39180c2c00b22c792d5b
unknown <deguzmankinlie@gmail.com>
2024-12-29 12:18:42 +0800

    counter in js

--- counter/counter.js
+++ counter/counter.js
@@ -1,23 +1,21 @@
-const decrease = document.getElementById('decrease');
+const decrease = document.getElementById("decrease");
 const reset = document.getElementById("reset");
 const increase = document.getElementById("increase");
 const count = document.getElementById("count");
 
 let counter = 0;
 
-decrease.onclick = function(){
-    counter--;
-    count.textContent = counter;
-}
-
-increase.onclick = function(){
-    counter++;
-    count.textContent = counter;
-}
-
-reset.onclick = function () {
-  counter=0;
+decrease.onclick = function () {
+  counter--;
   count.textContent = counter;
 };
 
+increase.onclick = function () {
+  counter++;
+  count.textContent = counter;
+};
 
+reset.onclick = function () {
+  counter = 0;
+  count.textContent = counter;
+};
