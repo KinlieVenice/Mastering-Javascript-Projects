0b3a0b4d3c48a0fee2c44538c8858db4a838527a
unknown <deguzmankinlie@gmail.com>
2024-12-31 20:12:35 +0800

    just practicing

--- practice.js
+++ practice.js
@@ -1,3 +1,11 @@
-function favNum(num){
-    return console.log(`My fav number is ${num}`)
+function largestNum(array) {
+  let largest = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0 && array[i] > largest) {
+      largest = array[i];
+    }
+  }
+  return largest;
 }
+
+console.log(largestNum([3, 33, 33, 6])); 
